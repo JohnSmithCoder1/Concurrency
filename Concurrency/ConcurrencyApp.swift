b608b132abc8408a65df1e09d9a5cd981d750ece
John Smith <johnsmithcoder1@gmail.com>
2024-10-23 09:23:02 -0500

    Initial Commit

---
+++ Concurrency/ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyApp.swift
+//  Concurrency
+//
+//  Created by J S on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
